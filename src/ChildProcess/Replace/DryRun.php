c4344f301cf7b7d9ff89abff0de594889b65d9cb
gong023 <gon.gong.gone@gmail.com>
2016-11-08 00:32:05 -0800

    create dryrun child process

---
+++ src/ChildProcess/Replace/DryRun.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace NamaeSpace\ChildProcess\Replace;
+
+use NamaeSpace\MutableString;
+use PhpParser\Node\Name;
+use React\EventLoop\LoopInterface;
+use SebastianBergmann\Diff\Differ;
+use WyriHaximus\React\ChildProcess\Messenger\ChildInterface;
+use WyriHaximus\React\ChildProcess\Messenger\Messages\Payload;
+use WyriHaximus\React\ChildProcess\Messenger\Messenger;
+
+class DryRun implements ChildInterface
+{
+    /**
+     * @var Payload
+     */
+    private $payload;
+
+    /**
+     * @var Differ
+     */
+    private $differ;
+
+    private function __construct(Payload $payload, Differ $differ)
+    {
+        $this->payload = $payload;
+        $this->differ = $differ;
+    }
+
+    public function process()
+    {
+        /** @var MutableString $code */
+        $code = \NamaeSpace\traverseToReplace(
+            file_get_contents($this->payload['real_path']),
+            $this->payload['origin_name'],
+            $this->payload['new_name']
+        );
+
+        if ($code->hasModification()) {
+            \NamaeSpace\writeln('<info>' . $this->payload['filename'] . '</info>');
+            \NamaeSpace\writeln($this->differ->diff($code->getOrigin(), $code->getModified()));
+        }
+    }
+
+    /**
+     * @param Messenger $messenger
+     * @param LoopInterface $loop
+     */
+    public static function create(Messenger $messenger, LoopInterface $loop)
+    {
+        $messenger->registerRpc('return', function (Payload $payload) {
+            $differ = new Differ("--- Original\n+++ New\n", false);
+
+            try {
+                (new self($payload, $differ))->process();
+                return \React\Promise\resolve();
+            } catch (\Exception $e) {
+                return \React\Promise\reject([
+                    'sent_payload'      => $payload->getPayload(),
+                    'exception_class'   => get_class($e),
+                    'exception_message' => $e->getMessage(),
+                ]);
+            }
+        });
+    }
+}

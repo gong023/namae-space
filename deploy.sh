9eb6780a0527bd6b16cc4e1bc0ab3cbd4dba3735
gong023 <gon.gong.gone@gmail.com>
2016-11-27 12:47:01 -0800

    fix script

--- deploy.sh
+++ deploy.sh
@@ -16,11 +16,7 @@ git remote add deploy git@github.com:gong023/namae-space.git
 git fetch deploy
 
 # Get box and build PHAR
-wget https://box-project.github.io/box2/manifest.json
-BOX_URL=$(php bin/parse-manifest.php manifest.json)
-rm manifest.json
-wget -O box.phar ${BOX_URL}
-chmod 755 box.phar
+curl -LSs https://box-project.github.io/box2/installer.php | php
 ./box.phar build -vv
 # Without the following step, we cannot checkout the gh-pages branch due to
 # file conflicts:
